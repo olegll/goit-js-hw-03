23c42953cebcd65cfec2648f896fe9cf62090c62
Oleh <olegduzh@gmail.com>
2024-11-15 15:02:11 +0200

    hw-3

---
+++ js/task-2.js
@@ -0,0 +1,17 @@
+const makeArray = function (firstArray, secondArray, maxLength) {
+    const sum = firstArray.concat(secondArray);
+    if (sum.length > maxLength) {
+        return sum.slice(0, maxLength);
+    } else {
+        return sum;
+    }
+    
+    
+}
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
